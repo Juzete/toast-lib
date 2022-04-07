710941f733a65b8991a380b5bf8a7685d9517a16
Maxim Korzenok <maximka00007777@gmail.com>
2022-04-07 09:59:46 +0300

    feat: add dnd errase toast

--- src/components/Toast/index.jsx
+++ src/components/Toast/index.jsx
@@ -7,6 +7,8 @@ import { ToastContainer, Icon, Text, Title, Cancel } from './components';
 const Toast = ({ properties }) => {
   const { removeToast, position } = useContext(ToastContext);
 
+  const [offsetX, setOffsetX] = useState(0);
+
   const { title, icon, text, id, fontSize, iconSize } = properties;
   console.log({ properties });
 
@@ -20,8 +22,25 @@ const Toast = ({ properties }) => {
     properties.remove(() => removeToast(id));
   };
 
+  const onDragStartHandler = (e) => {
+    const parent = e.target.parentNode.getBoundingClientRect();
+    setOffsetX(e.pageX - parent.left);
+  };
+
+  const onDragHandler = (e) => {
+    const parent = e.target.parentNode.getBoundingClientRect();
+    const event_offsetX = e.pageX - parent.left;
+    if (event_offsetX - offsetX === 100) remove();
+  };
+
   return (
-    <ToastContainer config={properties} position={position}>
+    <ToastContainer
+      config={properties}
+      position={position}
+      draggable={true}
+      onDrag={onDragHandler}
+      onDragStart={onDragStartHandler}
+    >
       <Title>{title}</Title>
       <Icon icon={icon} iconSize={iconSize}></Icon>
       <Text fontSize={fontSize}>{text}</Text>
